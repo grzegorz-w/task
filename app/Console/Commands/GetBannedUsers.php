1a63fe959eb7e03121cd623bb2cbec32554edd9c
Grzegorz W <gwygladala@gmail.com>
2023-03-09 21:12:15 +0100

    first refactor: - move logic to dedicated service. - use DI in CMD command

--- app/Console/Commands/GetBannedUsers.php
+++ app/Console/Commands/GetBannedUsers.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\User;
+use App\Service\BannedUser;
 use Illuminate\Console\Command;
 
 class GetBannedUsers extends Command
@@ -20,50 +21,9 @@ class GetBannedUsers extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(BannedUser $bannedUser)
     {
-        $query = null;
-
-        if($this->option('with-trashed')) {
-           $query = User::withTrashed();
-        } else if ($this->option('trashed-only')) {
-            $query = User::onlyTrashed();
-        } else {
-            $query = User::withoutTrashed();
-        }
-
-        $query = $query->whereNotNull('banned_at');
-
-        if($this->option('active-users-only')) {
-            $query = $query->whereNull('activated_at');
-        }
-
-        if($this->option('no-admin')) {
-            $query = $query->whereHas('roles', function (\Illuminate\Database\Eloquent\Builder $builder) {
-                $builder->where('name', '<>', 'ADMIN');
-            });
-        }
-
-        if($this->option('admin-only')) {
-            $query = $query->whereHas('roles', function (\Illuminate\Database\Eloquent\Builder $builder) { $builder->where('name', 'admin'); });
-        }
-
-        $o = [];
-        /** @var User $user */
-        foreach($query->get() as $user) {
-            $o[] = [
-                'email' => $user->email,
-                'banned_at' => $user->banned_at,
-                'id' => $user->id,
-            ];
-        }
-        $uu = '';
-        foreach ($o as $u) {
-            $uu .= $u['id']. ';'.$u['email'] . ";" . $u['banned_at'].PHP_EOL;
-        }
-        if($this->option('with-headers')) {
-            echo 'id;email;banned_at';
-        }
+        $uu = $bannedUser->getBannedUsers($this->option());
         echo ($uu);
         if($this->argument('save-to')) {
             file_put_contents(explode('=',$this->argument('save-to'))[1],$uu);
